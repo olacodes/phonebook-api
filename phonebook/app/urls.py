cb02ba42c3fcec4594d690db069088feb0117cef
olacodes <olatundesodiq@gmail.com>
2019-11-05 08:55:43 +0100

    add phonebook url

--- phonebook/app/urls.py
+++ phonebook/app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from phonebook.app import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('', views.PhonebookView)
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', include(router.urls))
 ]
