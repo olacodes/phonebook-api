f97eef4739dd0c73993239c4988fc2f4eda18929
olacodes <olatundesodiq@gmail.com>
2019-11-05 08:54:03 +0100

    add models to phonebook

--- phonebook/app/models.py
+++ phonebook/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Phonebook(models.Model):
+
+    lastname=models.CharField(max_length=255)
+    firstname=models.CharField(max_length=255)
+    phoneno=models.CharField(max_length=20)
+    email=models.CharField(max_length=255)
+    address=models.TextField(max_length=255)
+
+
