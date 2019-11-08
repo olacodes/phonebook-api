c3e09114a9c7c0a6f72f8be40cf45daea067c7a3
olacodes <olatundesodiq@gmail.com>
2019-11-08 19:01:50 +0100

    add api path

--- phonebook/urls.py
+++ phonebook/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/', include('phonebook.app.urls')),
+    path('api/', include('phonebook.app.urls'), name="api"),
     path('', admin.site.urls)
 ]
