9fb18520b259c5de8f856e9773fc8be75798338d
olacodes <olatundesodiq@gmail.com>
2019-11-08 19:08:02 +0100

    modify serializers

--- phonebook/app/serializers.py
+++ phonebook/app/serializers.py
@@ -4,7 +4,4 @@ from .models import Phonebook
 class PhonebookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Phonebook
-        fields = ("id", "lastname", "firstname", "phoneno", "email", "address")
-
-    
-
+        fields = ["id", "lastname", "firstname", "phone_number", "email", "address"]
