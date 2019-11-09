1819b56b9ab42f3c6f245c8040e34ac908593013
olacodes <olatundesodiq@gmail.com>
2019-11-09 19:13:57 +0100

    move files

--- phonebook/app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Phonebook
-
-class PhonebookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Phonebook
-        fields = ["id", "lastname", "firstname", "phone_number", "email", "address"]
